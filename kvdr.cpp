ca0d6563c1fa202c63d5523ecc32fc2bd0299b2f
petrgot2406 <p3trgot@yandex.ru>
2024-08-20 23:28:31 +0300

    Разбил на функции нулёвую прогу

--- kvdr.cpp
+++ kvdr.cpp
@@ -1,35 +1,81 @@
 #include <stdio.h>
 #include <math.h>
 #include <TXLib.h>
-main() {
-    float a, b, c;
-    printf("Введите a, b, c: ");
+
+const float EPS = 1e-9f;
+const int INF_NUM_OF_ROOTS = -1;
+
+float Discriminant(float a, float b, float c);
+bool IsClose(float x, float y);
+void SolveQuad(float a, float b, float c, int* colvo, float* x1, float* x2);
+void LinUravn(float b, float c, int* colvo, float* x1);
+void AnswerOutput(int* colvo, float* x1, float* x2);
+
+int main() {
+    float a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+    int colvo = 0;
+    printf("Р’РІРµРґРёС‚Рµ a, b, c: ");
     scanf("%f %f %f", &a, &b, &c);
-    if (a == 0) {
-        if (b == 0) {
-            if (c == 0) {
-                printf("x - любое число");
-            } else {
-                printf("корней нет");
-            }
-        } else {
-            printf ("x = %g", - c / b);
-        }
+    SolveQuad(a, b, c, &colvo, &x1, &x2);
+    AnswerOutput(&colvo, &x1, &x2);
+    return 0;
+}
+
+float Discriminant(float a, float b, float c) {
+    return b * b - 4 * a * c;
+}
+
+bool IsClose(float x, float y) {
+    if (fabs(x - y) < EPS) {
+        return true;
+    }
+    return false;
+}
+
+void SolveQuad(float a, float b, float c, int* colvo, float* x1, float* x2) {
+    if (IsClose(a, 0)) {
+        LinUravn(b, c, colvo, x1);
     } else {
-        float D, x1, x2;
-        D = b * b - 4 * a * c;
+        float D = 0;
+        D = Discriminant(a, b, c);
         if (D < 0) {
-            printf("корней нет");
+            *colvo = 0;
         } else {
-            if (D == 0) {
-                x1 = - b / (2 * a);
-                printf("x = %g", x1);
+            if (IsClose(D, 0)) {
+                *colvo = 1;
+                *x1 = - b / (2 * a);
             } else {
-                x1 = (- b + sqrt(D)) / (2 * a);
-                x2 = (- b - sqrt(D)) / (2 * a);
-                printf("x1 = %g, x2 = %g", x1, x2);
+                *colvo = 2;
+                *x1 = (- b - sqrtf(D)) / (2 * a);
+                *x2 = (- b + sqrtf(D)) / (2 * a);
+                printf("%g, %g\n", *x1, *x2);
             }
         }
     }
-    return 0;
+}
+
+void LinUravn(float b, float c, int* colvo, float* x1) {
+    if (IsClose(b, 0)) {
+        if (IsClose(c, 0)) {
+            *colvo = INF_NUM_OF_ROOTS;
+        } else {
+            *colvo = 0;
+        }
+    } else {
+        *colvo = 1;
+        *x1 = - c / b;
+    }
+}
+
+void AnswerOutput(int* colvo, float* x1, float* x2) {
+    switch(*colvo) {
+        case 0: printf("РЅРµС‚ СЂРµС€РµРЅРёР№");
+                break;
+        case 1: printf("x = %g", *x1);
+                break;
+        case 2: printf("x1 = %g, x2 = %g", *x1, *x2);
+                break;
+        case INF_NUM_OF_ROOTS: printf("Р»СЋР±РѕРµ С‡РёСЃР»Рѕ");
+                               break;
+    }
 }
