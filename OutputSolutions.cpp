28a985805b828be1f94555ae1910574129d30ae5
petrgot2406 <p3trgot@yandex.ru>
2024-10-12 04:49:42 +0300

    Replaced some output words

--- OutputSolutions.cpp
+++ OutputSolutions.cpp
@@ -8,13 +8,13 @@ void AnswerOutput(Roots sol)
 {
     switch(sol.nRoots)
     {
-        case 0: printf("нет решений\n");
+        case 0: printf("No solutions\n");
                 break;
         case 1: printf("x = %g\n", sol.x1);
                 break;
         case 2: printf("x1 = %g, x2 = %g\n", sol.x1, sol.x2);
                 break;
-        case INF_NUM_OF_ROOTS: printf("любое число\n");
+        case INF_NUM_OF_ROOTS: printf("Any number\n");
                                break;
 
         default: txSetConsoleAttr (FOREGROUND_RED | BACKGROUND_BLACK);
